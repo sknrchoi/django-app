8da4948e733a0deb27ee71a6fc1605150df7052e
sknrchoi <seulkinara@gmail.com>
2019-02-26 17:16:17 +0900

    django app, first commit

---
+++ python_intro.py
@@ -0,0 +1,29 @@
+# print('Hello, Django')
+
+# if 3 > 2:
+#     print('it works!')
+# else :
+#     print('3 is not greater than 2')
+
+
+def hi(name) :
+    # if name == 'Ola':
+    #     print('Hi Ola!')
+    # elif name == 'Sonja':
+    #     print('Hi Sonja!')
+    # else:
+    #     print('Hi anonymous!');
+    
+    print('Hi ' + name + '!')
+           
+
+girls = ['Ola', 'Sonja', 'You', 'Phoebe']
+
+for name in girls:
+    hi(name)
+    print('Next girl')
+
+for i in range(1, 3):
+    print(i)
+
+
