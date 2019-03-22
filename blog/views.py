cff093e38e1d9c5302265f488c2e6c1e480170c3
sknrchoi <seulkinara@gmail.com>
2019-03-22 13:45:44 +0900

    Added vies to create/edit blog post inside the site

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -13,4 +15,30 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-    # Post.object.get(pk=pk)
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+
+        if form.is_valid() :
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save() # 변경사항을 유지하고 새 블로그 글이 만들어짐
+            return redirect('post_detail', pk=post.pk) # 상세페이지로 이동
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk) :
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form':form})
